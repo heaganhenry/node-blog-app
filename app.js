e9f8308264da7f3055cff776114abfa759e58757
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2023-01-01 09:23:06 -0400

    Setup db with mongoose

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.listen(process.env.PORT);
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_URI)
+    .then(result => app.listen(process.env.PORT))
+    .catch(error => console.log(error));
 
 app.get('/', (req, res) => {
     const blogs = [

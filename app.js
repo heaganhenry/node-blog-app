85cb530f26384e21e10319e766ffe893a251929b
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2023-01-01 08:47:15 -0400

    Create index, about and 404 routes

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ app.listen(3000);
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    const blogs = [
+        { title: 'Blog 1', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Blog 2', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Blog 3', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
+    res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.use((req, res) => {
+    res.render('404', { title: '404' });
+});
